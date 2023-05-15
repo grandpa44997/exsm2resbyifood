805b7f8a50ec32e9edfa1d674947191ab77d1c44
Daniel Freitas <bikemessengerateu@gmail.com>
2023-05-14 22:39:15 -0300

    Update ex10m2.py
    refazendo

--- ex10m2.py
+++ ex10m2.py
@@ -1,10 +1,16 @@
-'''Crie uma classe Aluno que tenha os atributos nome, idade e matrícula. Em seguida, crie um objeto da classe Aluno, leia os valores de nome, 
-idade e matrícula desse objeto do usuário e exiba na tela os dados do aluno cadastrado.'''
-
+'''Crie uma classe Aluno que tenha os atributos nome, idade e matrícula. 
+Em seguida, crie um objeto da classe Aluno, leia os valores de nome, idade 
+e matrícula desse objeto do usuário e exiba na tela os dados do aluno 
+cadastrado.'''
+# Criação da classe Aluno
 class Aluno:
+    # construtor da classe Aluno
     def __init__(self, nome, idade, matricula):
+        # atributo nome
         self.nome = nome
+        # atributo idade
         self.idade = idade
+        # atributo matrícula
         self.matricula = matricula
 
 # Criação do objeto da classe Aluno
@@ -17,6 +23,6 @@ aluno.matricula = input('Digite a matrícula do aluno: ')
 
 # Exibição dos dados do aluno cadastrado
 print('Dados do aluno cadastrado:')
-print('Nome:', aluno.nome)
-print('Idade:', aluno.idade)
-print('Matrícula:', aluno.matricula)
+print('Nome:', aluno.nome) # print e acesso ao atributo nome
+print('Idade:', aluno.idade) # print e acesso ao atributo idade
+print('Matrícula:', aluno.matricula) # print e acesso ao atributo matrícula
